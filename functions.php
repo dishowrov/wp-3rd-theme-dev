37ac244b8104bb2c3cc9688ea5e58226ad178e9b
dishowrov <dinislamshowrov.2001@gmail.com>
2024-01-30 13:03:25 +0600

    linked all css of markeup folder

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function fita_theme_setup(){
+function fita_theme_setup()
+{
     load_theme_textdomain("fita");
     add_theme_support("post-thumbnails");
     add_theme_support("title-tag");
@@ -8,9 +9,11 @@ function fita_theme_setup(){
 add_action("after_setup_theme", "fita_theme_setup");
 
 
-function fita_assets() {
-
+function fita_assets()
+{
+    wp_enqueue_style("animate-css", get_theme_file_uri("/assets/css/animate.css"));
+    wp_enqueue_style("icomoon-css", get_stylesheet_uri("/assets/css/icomoon.css"));
+    wp_enqueue_style("bootstrap-css", get_stylesheet_uri("/assets/css/bootstrap.css"));
+    wp_enqueue_style("custom-style-css", get_stylesheet_uri("/assets/css/style.css"));
 }
 add_action("wp_enqueue_scripts", "fita_assets");
-
-?>
