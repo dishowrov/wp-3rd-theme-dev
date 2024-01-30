bc077219caa04ea2e27886f25be545e571462b65
dishowrov <dinislamshowrov.2001@gmail.com>
2024-01-30 14:30:23 +0600

    linked all js files

--- functions.php
+++ functions.php
@@ -16,5 +16,11 @@ function fita_assets()
     wp_enqueue_style("bootstrap-css", get_stylesheet_uri("/assets/css/bootstrap.css"));
     wp_enqueue_style("custom-style-css", get_stylesheet_uri("/assets/css/style.css"));
     wp_enqueue_style("fita", get_stylesheet_uri());
+
+    wp_enqueue_script("jquery-easing-js", get_theme_file_uri("/assets/js/jquery.easing.1.3.js"), array("jquery"), null, true);
+    wp_enqueue_script("bootstrap-js", get_theme_file_uri("/assets/js/bootstrap.min.js"), array("jquery"), null,true);
+    wp_enqueue_script("jquery-waypoints", get_theme_file_uri("/assets/js/jquery.waypoints.min.js"), array("jquery"), null, true);
+    wp_enqueue_script("custom-js", get_theme_file_uri("/assets/js/main.js"), array("jquery"), null, true
+);
 }
 add_action("wp_enqueue_scripts", "fita_assets");
