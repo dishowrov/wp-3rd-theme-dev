af1dfd0b26e5ef4e9b34622c883cd38e03e4ba3d
dishowrov <dinislamshowrov.2001@gmail.com>
2024-01-30 15:43:17 +0600

    registered 2 sidebar

--- functions.php
+++ functions.php
@@ -23,3 +23,32 @@ function fita_assets()
     wp_enqueue_script("custom-js", get_theme_file_uri("/assets/js/main.js"), array("jquery"), null, true);
 }
 add_action("wp_enqueue_scripts", "fita_assets");
+
+
+function fita_sidebars()
+{
+    register_sidebar(
+        array(
+            'name' => __('Footer Left', 'fita'),
+            'id' => 'footer-left',
+            'description' => __('Footer left widget', 'fita'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget' => '</section>',
+            'before_title' => '<h2 class="widget-title>',
+            'after_title' => '</h2>'
+        )
+    );
+
+    register_sidebar(
+        array(
+            'name' => __('Footer Right', 'fita'),
+            'id' => 'footer-right',
+            'description' => __('Footer right widget', 'fita'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget' => '</section>',
+            'before_title' => '<h2 class="widget-title>',
+            'after_title' => '</h2>'
+        )
+    );
+}
+add_action("widgets_init", "fita_sidebars");
